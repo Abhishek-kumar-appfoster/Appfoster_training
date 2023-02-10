284df9008fb9386f549466af9f3230250a3d3b9e
Mona LisaAbhishek-Kumargit config --global user.nameclear <abhishek.kumar@appfoster.com>
2023-02-10 11:03:45 +0530

    #3293:User details like name, email , gender are getting fetched

--- task-10-async-Js/scripts/script.js
+++ task-10-async-Js/scripts/script.js
@@ -1,35 +1,54 @@
 
-
-
 async function showUsers() {
     fetch("https://gorest.co.in/public/v2/users")
-      .then((data) => {
-        return data.json();
-      })
-      .then((objectData) => {
-        let tableData = "";
-        objectData.map((values) => {
-          tableData += `<tr>
+        .then((data) => {
+            return data.json();
+        })
+        .then((objectData) => {
+            let tableData = "";
+            objectData.map((values) => {
+                tableData += `<tr>
             <td>${values.name}</td>
-            <td><button class="btn btn-primary" data-toggle="modal" data-target="#userModal" onclick="displayDetails('${values.name}', '${values.email}')">View More</button></td>
+            <td><button class="btn btn-primary" data-toggle="modal" data-target="#userModal" onclick="displayDetails('${values.id}')">View More</button></td>
           </tr>`;
-        });
-        document.getElementById("tableBody").innerHTML = tableData;
-      })
-      .catch((err) => {
+            });
+            document.getElementById("tableBody").innerHTML = tableData;
+        })
+        .catch((err) => {
+            console.log("error occured!");
+        })
+        .finally(console.log("done fine"));
+}
+showUsers();
+
+function displayDetails(id) {
+    fetch("https://gorest.co.in/public/v2/users/" + id)
+    .then((data) => {
+        return data.json();
+        console.log(data);
+    })
+    .then((objectData) => {
+        console.log(objectData);
+        document.getElementById("name").innerHTML = objectData.name;
+       
+    document.getElementById("email").innerHTML = objectData.email;
+    document.getElementById("gender").innerHTML = objectData.gender;
+        
+    })
+    .catch((err) => {
+        // console.log(data);
+         console.log(err);
         console.log("error occured!");
-      })
-      .finally(console.log("done fine"));
-  }
-  showUsers();
-  
-  function displayDetails(name, email) {
-    document.getElementById("modalName").innerHTML = name;
-    document.getElementById("modalEmail").innerHTML = email;
-  }
-  
-  
-  
-  
-  
-  
+    })
+    .finally(console.log("done fine"));
+}
+    
+
+
+
+
+
+
+
+
+
