c13e5bbb7185f9e36b9fc638370885684763ba77
Mona LisaAbhishek-Kumargit config --global user.nameclear <abhishek.kumar@appfoster.com>
2023-02-20 11:03:32 +0530

    Task-10: jquery added instead for onclick method

--- task-10-async-Js/scripts/script.js
+++ task-10-async-Js/scripts/script.js
@@ -1,81 +1,47 @@
+$(document).ready(function() {
 
-async function showUsers() {
-    fetch("https://gorest.co.in/public/v2/users")
-        .then((data) => {
-            return data.json();
-        })
-        .then((objectData) => {
-            let tableData = "";
-            objectData.map((values) => {
-                tableData += `<tr>
-            <td>${values.name}</td>
-            <td><button class="btn btn-primary" data-toggle="modal" data-target="#userModal" onclick="displayDetails('${values.id}')">View More</button></td>
-          </tr>`;
-            });
-            document.getElementById("tableBody").innerHTML = tableData;
-        })
-        .catch((err) => {
-            console.log("error occured!");
-        })
-        .finally(console.log("done fine"));
-}
-showUsers();
+    function showUsers() {
+        fetch("https://gorest.co.in/public/v2/users")
+            .then((data) => {
+                return data.json();
+            })
+            .then((objectData) => {
+                let tableData = "";
+                objectData.map((values) => {
+                    tableData += `<tr>
+                <td>${values.name}</td>
+                <td><button class="btn btn-primary view-more-btn" data-toggle="modal" data-target="#userModal" data-id="${values.id}">View More</button></td>
+              </tr>`;
+                });
+                $("#tableBody").html(tableData);
+            })
+            .catch((err) => {
+                console.log("error occured!");
+            })
+            .finally(() => console.log("done fine"));
+    }
+    showUsers();
 
-function displayDetails(id) {
-    fetch("https://gorest.co.in/public/v2/users/" + id)
-        .then((data) => {
-            return data.json();
-            console.log(data);
-        })
-        .then((objectData) => {
-            console.log(objectData);
-            document.getElementById("name").innerHTML = objectData.name;
+    function displayDetails(id) {
+        fetch("https://gorest.co.in/public/v2/users/" + id)
+            .then((data) => {
+                return data.json();
+            })
+            .then((objectData) => {
+                $("#name").text(objectData.name);
+                $("#email").text(objectData.email);
+                $("#gender").text(objectData.gender);
+            })
+            .catch((err) => {
+                console.log(err);
+                console.log("error occured!");
+            })
+            .finally(() => console.log("done fine"));
+    }
 
-            document.getElementById("email").innerHTML = objectData.email;
-            document.getElementById("gender").innerHTML = objectData.gender;
+    $(document).on("click", ".view-more-btn", function() {
+        const userId = $(this).data("id");
+        displayDetails(userId);
+    });
 
-        })
-        .catch((err) => {
-            // console.log(data);
-            console.log(err);
-            console.log("error occured!");
-        })
-        .finally(console.log("done fine"));
-}
-                    //    post-details
-
-
-// function displayJobs(id){
-//     fetch("https://gorest.co.in/public/v2/users/${id}/posts")
-//     .then((data) => {
-//         return data.json();
-        
-//     })
-//     .then((objectData) => {
-//         console.log(objectData);
-//         document.getElementById("name").innerHTML = objectData.name;
-
-//         document.getElementById("email").innerHTML = objectData.email;
-//         document.getElementById("gender").innerHTML = objectData.gender;
-
-//     })
-//     .catch((err) => {
-//         // console.log(data);
-//         console.log(err);
-//         console.log("error occured!");
-//     })
-//     .finally(console.log("done fine"));
-
-   
-// }
-// displayJobs(359769);
-
-
-
-
-
-
-
-
-
-
+});
